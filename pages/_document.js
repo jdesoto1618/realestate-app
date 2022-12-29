dcf93bbf800b607ba49cc6cb742e2f919ab057d1
Joe DeSoto <modulusman@gmail.com>
2022-12-28 16:02:14 -0800

    Rolls back chakra ui to a working version with next 13 along with its corresponding dependencies.
    Brings in inter font as link tags and sets the body to Inter.

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossorigin
+      ></link>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+        rel="stylesheet"
+      ></link>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
