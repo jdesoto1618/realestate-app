be9f9f246dae9b9081bf9f183990fb2ee773b53b
Joe DeSoto <modulusman@gmail.com>
2022-12-28 17:29:37 -0800

    Adds images to index.js and also sets configuration in next config to allow the image domains.
    Also places the Banner component on the page twice, once for renting
    and the other for buying.

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,40 @@ import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button, Spacer } from "@chakra-ui/react";
 
-const Banner = ({ purpose }) => (
+const Banner = ({
+  purpose,
+  title1,
+  title2,
+  desc1,
+  desc2,
+  buttonText,
+  linkName,
+  imageUrl,
+}) => (
   <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
-    {purpose}
+    <Image src={imageUrl} width={500} height={300} alt="banner image" />
+    <Box p="5">
+      <Text color="gray.500" fontSize="sm" fontWeight="medium">
+        {purpose}
+      </Text>
+      <Text fontSize="3xl" fontWeight="bold">
+        {title1} <br /> {title2}
+      </Text>
+      <Text
+        color="gray.700"
+        fontSize="lg"
+        paddingTop="3"
+        paddingBottom="3"
+        fontWeight="medium"
+      >
+        {desc1}
+        <br />
+        {desc2}
+      </Text>
+      <Button fontSize="xl" bg="blue.300" color="white">
+        <Link href={linkName}>{buttonText}</Link>
+      </Button>
+    </Box>
   </Flex>
 );
 
@@ -12,8 +43,26 @@ export default function Home() {
   return (
     <>
       <h1>Hello, World!</h1>
-      <Banner purpose={"For Sale"} />
-      <Banner purpose={"For Rent"} />
+      <Banner
+        purpose="Rent a home"
+        title1="Rental homes for"
+        title2="Everyone"
+        desc1="Explore Apartments, Villas, Homes"
+        desc2="and more"
+        buttonText="Explore Renting"
+        linkName="/search?purpose=for-rent"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+      />
+      <Banner
+        purpose="Buy a home"
+        title1="Find, buy, and own your"
+        title2="dream home"
+        desc1="Explore Apartments, Villas, Homes"
+        desc2="and more"
+        buttonText="Explore Buying"
+        linkName="/search?purpose=for-rent"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
+      />
     </>
   );
 }
