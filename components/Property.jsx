3b5031d40d27ace2359e25dfd14f135b49f16ddb
Joe DeSoto <modulusman@gmail.com>
2023-02-28 14:03:34 -0800

    Finishes setup for property cards and adds more styling to help the card layout make more UI sense.

--- components/Property.jsx
+++ components/Property.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
 import { FaBed, FaBath } from "react-icons/fa";
-import { BsGridFill } from "react-icons/bs";
+import { BsGrid, BsGridFill } from "react-icons/bs";
 import { GoVerified } from "react-icons/go";
 import millify from "millify";
 import DefaultImage from "../assets/images/house.webp";
@@ -25,7 +25,6 @@ const Property = ({
     <Flex
       flexWrap="wrap"
       w="420px"
-      p="5"
       paddingTop="0"
       justifyContent="flex-start"
       cursor="pointer"
@@ -33,10 +32,46 @@ const Property = ({
       <Box>
         <Image
           src={coverPhoto ? coverPhoto.url : DefaultImage}
-          width={400}
+          width={410}
           height={260}
           alt="house image"
         />
+      </Box>
+
+      <Box w="full" display={"flex"} flexDirection={"column"} gap={24}>
+        <Flex
+          paddingTop="2"
+          alignItems="center"
+          justifyContent="space-between"
+          flex-direction={"column"}
+          gap="12px"
+        >
+          <Flex alignItems="center">
+            <Box paddingRight="3" color="green.400">
+              {isVerified && <GoVerified />}
+            </Box>
+            <Text fontWeight="bold" fontSize="lg">
+              AED {millify(price)}
+              {rentFrequency && `/${rentFrequency}`}
+            </Text>
+          </Flex>
+          <Box textAlign={"center"}>
+            <Avatar size="sm" src={agency?.logo?.url} maxWidth="50%" />
+          </Box>
+        </Flex>
+
+        <Flex
+          alignItems="center"
+          justifyContent={"center"}
+          gap="16"
+          color="blue.400"
+        >
+          {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft
+          <BsGridFill />
+        </Flex>
+        <Text fontSize={"lg"} textAlign={"center"}>
+          {title.length > 30 ? `${title.substring(0, 30)}...` : title}
+        </Text>
       </Box>
     </Flex>
   </Link>
